51cf0f6b2af3352346beb6c5dabd2e2445a8b22c
Towa Tiemeni Frank Brayan <franktowa199724@gmail.com>
2023-07-04 15:50:10 +0200

    Update votes.js

--- frontend/src/Composants/Votes/votes.js
+++ frontend/src/Composants/Votes/votes.js
@@ -26,7 +26,7 @@ const App = () => {
   };
 
   return (
-    <Grid container spacing={3} justifyContent="center" paddingTop={5}>
+    <Grid container spacing={2} justifyContent="center" paddingTop={5}>
       <Grid item xs={12}>
         <Typography variant="h4" align="center" gutterBottom>
           Votes des Joueurs
